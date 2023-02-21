c39712fc9ed1a2672f7228d882d79b7b28f54b47
Emre Özay <emreozay@mail.com>
2023-02-21 13:37:07 +0300

    Digging animations added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float moveSpeed;
 
+    [SerializeField]
+    private GameObject hole;
+    [SerializeField]
+    private GameObject pouch;
+
+    private bool isMoving = true;
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
@@ -31,8 +38,11 @@ public class PlayerController : MonoBehaviour
 
     private void MoveWithJoystick()
     {
+        if (!isMoving)
+            return;
+
         playerRigidbody.velocity = new Vector2(floatingJoystick.Horizontal, floatingJoystick.Vertical).normalized * moveSpeed;
-        
+
         if (floatingJoystick.Horizontal != 0 || floatingJoystick.Vertical != 0)
         {
             animator.SetBool("isRunning", true);
@@ -43,5 +53,28 @@ public class PlayerController : MonoBehaviour
         {
             animator.SetBool("isRunning", false);
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerRigidbody.velocity = Vector2.zero;
+        isMoving = false;
+
+        collision.gameObject.SetActive(false);
+
+        StartCoroutine(Dig());
+    }
+
+    private IEnumerator Dig()
+    {
+        animator.SetTrigger("isDigging");
+        Instantiate(hole, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2f);
+        pouch.transform.position = transform.position;
+        pouch.SetActive(true);
+        
+        yield return new WaitForSeconds(1f);
+        pouch.SetActive(false);
+        isMoving = true;
     }
 }
